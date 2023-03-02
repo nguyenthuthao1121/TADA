8de298cad8b4ed9dd3bc5cf99b32d37601ea003d
ThoaiVy <truongthoaivy3102003@gmail.com>
2023-03-02 14:59:47 +0700

    Add UI user, change index.cshtml

---
+++ Pages/Guest/ForgotPwd.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TADA.Pages;
+
+public class ForgotPwdModel : PageModel
+{
+    private readonly ILogger<ForgotPwdModel> _logger;
+
+    public ForgotPwdModel(ILogger<ForgotPwdModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
