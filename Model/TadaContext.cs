272703c9a09fd5e01150b170a13a9431eb0b43a3
ThoaiVy <truongthoaivy3102003@gmail.com>
2023-04-03 18:31:16 +0700

    Create Model ver1

--- Model/TadaContext.cs
+++ Model/TadaContext.cs
@@ -9,7 +9,22 @@ namespace TADA.Model
         public TadaContext(DbContextOptions<TadaContext> option) :base(option) { 
             
         }
-        DbSet<Book> Books { get; set; }
+        DbSet<Book> books { get; set; }
+        DbSet<Provider> providers { get; set; }
+        DbSet<Contract> contracts { get; set; }
+        DbSet<Category> categories { get; set; }
+        DbSet<Order> orders { get; set; }
+        DbSet<Status> statuses { get; set; }
+        DbSet<Customer> customers { get; set; }
+        DbSet<Staff> staffs { get; set; }
+        DbSet<Role> roles { get; set; }
+        DbSet<Account> accounts { get; set; }
+        DbSet<Cart> carts { get; set; }
+        DbSet<Review> reviews { get; set; }
+        DbSet<Province> provinces { get; set; }
+        DbSet<District> districts { get; set; }
+        DbSet<Ward> wards { get; set; }
+        DbSet<Address> addresses { get; set; }
 
     }
 }
