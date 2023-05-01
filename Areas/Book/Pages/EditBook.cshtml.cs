bb34d65fb818491cf6f102a97ecb389313ec8a9d
ThoaiVy <truongthoaivy3102003@gmail.com>
2023-05-01 21:33:09 +0700

    update ver 1

--- Areas/Book/Pages/EditBook.cshtml.cs
+++ Areas/Book/Pages/EditBook.cshtml.cs
@@ -1,19 +1,51 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TADA.Dto.Book;
+using TADA.Dto.Provider;
+using TADA.Dto.Review;
+using TADA.Service;
 
 namespace TADA.Pages;
 
 public class EditBookModel : PageModel
 {
-    private readonly ILogger<EditBookModel> _logger;
+    private readonly IBookService bookService;
+    private readonly IProviderService providerService;
 
-    public EditBookModel(ILogger<EditBookModel> logger)
+    [BindProperty]
+    public BookDto Book { get; set; }
+    [BindProperty]
+    public string DescriptionText { get; set; }
+    [BindProperty]
+    public string ImagePath { get; set; }
+    public List<ProviderManagementDto> Providers { get; set; }
+
+    public EditBookModel(IBookService bookService, IProviderService providerService)
     {
-        _logger = logger;
+        this.bookService = bookService;
+        this.providerService = providerService;
     }
-
     public void OnGet()
     {
+        Providers = providerService.GetAllProviders();
+        if (int.TryParse(Request.Query["id"], out int bookId))
+        {
+            Book = bookService.GetBookById(bookId);
+            DescriptionText = "";
+            var filePath = Book.Description;
+            if (System.IO.File.Exists(filePath))
+            {
+                using (StreamReader sr = new StreamReader(filePath))
+                {
+                    DescriptionText = sr.ReadToEnd();
+                    sr.Close();
+                }
+            }
+        }
 
+    }
+    public IActionResult OnPostChangeBookInformation()
+    {
+        return RedirectToPage("./BookDetailAdmin");
     }
 }
--- Dto/Book/BookDto.cs
+++ Dto/Book/BookDto.cs
@@ -8,20 +8,45 @@ namespace TADA.Dto.Book;
 public class BookDto
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập tên sách!")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập tên tác giả!")]
+
     public string Author { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập tên nhà xuất bản!")]
+
     public string Publisher { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập năm xuất bản!")]
+    [RegularExpression(@"^(19|20)\d{2}$", ErrorMessage = "Vui lòng nhập năm hợp lệ!")]
     public int PublicationYear { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập thể loại!")]
+
     public string Genre { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập số trang!")]
+    [RegularExpression(@"^(?=.*[1-9])\d+$", ErrorMessage = "Vui lòng nhập số trang hợp lệ!")]
+
     public int Pages { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập chiều dài!")]
+    [RegularExpression(@"^(?=.*[1-9])\d*(\.\d+)?$", ErrorMessage = "Vui lòng nhập chiều dài hợp lệ!")]
     public double Length { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập chiều rộng!")]
+    [RegularExpression(@"^(?=.*[1-9])\d*(\.\d+)?$", ErrorMessage = "Vui lòng nhập chiều rộng hợp lệ!")]
+
     public double Width { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập trọng lượng!")]
+    [RegularExpression(@"^(?=.*[1-9])\d*(\.\d+)?$", ErrorMessage = "Vui lòng nhập trọng lượng hợp lệ!")]
     public double Weight { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập số trang!")]
+    [RegularExpression(@"^(?=.*[1-9])\d+$", ErrorMessage = "Vui lòng nhập giá tiền hợp lệ!")]
     public int Price { get; set; }
     public string Cover { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập số trang!")]
+    [RegularExpression(@"^(?=.*[1-9])\d+$", ErrorMessage = "Vui lòng nhập số lượng hợp lệ!")]
     public int Quantity { get; set; }
     public string Description { get; set; }
     public string Image { get; set; }
+    [Required(ErrorMessage = "Vui lòng nhập số trang!")]
+    [RegularExpression(@"^(?=.*[1-9])\d+$", ErrorMessage = "Vui lòng nhập số phần trăm giảm giá hợp lệ!")]
     public int Promotion { get; set; }
     public string CategoryName { get; set; }
     public string ProviderName { get; set; }
